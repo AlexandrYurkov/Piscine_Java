413cecee4a80a0e9869903c4c15e2d0527218c3b
aleksandryurkov <Aleksandr.Yurkov@infotecs.ru>
2023-08-02 18:32:55 +0300

    ex02

--- javaPiscine/day02/ex02/src/Program.java
+++ javaPiscine/day02/ex02/src/Program.java
@@ -1,5 +1,47 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Program {
+    private static final String CURRENT_FOLDER = "--current-folder=";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Path homeDir = Paths.get("/");
+        if (args.length != 0 && args[0].startsWith(CURRENT_FOLDER)){
+            homeDir = Paths.get(args[0].substring(CURRENT_FOLDER.length()));
+        }
+
+        CommandLine cmdL = new CommandLine(homeDir.normalize());
+        Scanner scanner = new Scanner(System.in);
+        String userInput;
+        System.out.print("-> ");
+        while ((userInput = scanner.nextLine()) != null) {
+            try {
+                String[] command = userInput.split(" ");
+                switch (command[0]) {
+                    case "ls":
+                        cmdL.ls();
+                        break;
+                    case "cd":
+                        cmdL.cd(Paths.get(command[1]));
+                        break;
+                    case "pwd":
+                        cmdL.pwd();
+                        break;
+                    case "mv":
+                        cmdL.mv(Paths.get(command[1]), Paths.get(command[2]));
+                        break;
+                    case "exit":
+                        scanner.close();
+                        System.exit(0);
+                    default:
+                        System.out.println("Unknown command. For exit type \"exit\"");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.print("-> ");
+        }
     }
 }

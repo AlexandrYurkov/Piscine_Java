bfb74d9ee8831ce77cf1c877968c443e90ae12b1
aleksandryurkov <Aleksandr.Yurkov@infotecs.ru>
2023-08-07 18:33:37 +0300

    EX02

--- javaPiscine/day03/ex02/src/Program.java
+++ javaPiscine/day03/ex02/src/Program.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Program {
@@ -24,14 +25,15 @@ public class Program {
         private int[] array;
         private Integer result = 0;
 
-        private int ThreadsCount = 0;
+        private int threadsCount = 0;
         Summa sum;
 
-        public Totalizer(int start, int end, int[] array, Summa summa) {
+        public Totalizer(int threadsCount, int start, int end, int[] array, Summa summa) {
             this.start = start;
             this.end = end;
             this.array = array;
             this.sum = summa;
+            this.threadsCount = threadsCount;
         }
 
         public int getResult() {
@@ -42,14 +44,13 @@ public class Program {
             for(int i = start; i < end; i++){
                 result += array[i];
             }
-            System.out.println("Summa total = " + sum.getSumma());
             sum.setSumma(result);
         }
 
         @Override
         public void run() {
             totalizer();
-            System.out.printf("Thread %d: from %d to %d sum is %d\n", this.ThreadsCount, start, end, result);
+            System.out.printf("Thread %d: from %d to %d sum is %d\n", this.threadsCount, start, end, result);
         }
     }
 
@@ -66,19 +67,8 @@ public class Program {
         return result;
     }
 
-    private static int[] step(int arraySize, int threadsCount){
-         int step = (int)Math.round((double) arraySize / threadsCount);
-         int[] steps = new int[threadsCount];
-         for (int i = 0; i < threadsCount; i++) {
-             if (i == 0)
-                 steps[i] = 0;
-             else
-                 if(steps[i-1] + step <= arraySize)
-                    steps[i] = steps[i-1] + step;
-                 else
-                     steps[i] = arraySize;
-         }
-         return steps;
+    private static int step(int arraySize, int threadsCount){
+         return (int)Math.round((double) arraySize / threadsCount);
     }
     private static int checkSum(int[] result){
         int checkSum = 0;
@@ -86,29 +76,56 @@ public class Program {
         return checkSum;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        int arraySize = parserInt(args[0]);
-        int threadsCount = parserInt(args[1]);
-        int[] result = generator(arraySize);
-        int[] steps = step(arraySize, threadsCount);
-        Summa summa = new Summa(0);
-        for (int i = 0; i < threadsCount;  i+=2){
-            Totalizer count = new Totalizer(steps[i], steps[i+1], result, summa);
-            System.out.println(count.start + ": " + count.end);
-            Thread thread = new Thread(count);
-            thread.start();
-            System.out.println(count.getResult());
-            if(threadsCount%2 == 1){
-                Totalizer tail = new Totalizer(steps[threadsCount-2], steps[threadsCount-1], result, summa);
-                Thread threadTail = new Thread(tail);
-                threadTail.start();
-                threadTail.join();
-                System.out.println("tail");
+    private static ArrayList<Totalizer> total(int steps, int threadsCount, int[] result, Summa summa){
+        ArrayList<Totalizer> total = new ArrayList<>();
+        int tailSteps = 0;
+        int nextSteps = steps;
+        for (int i = 0; i < threadsCount;  i++){
+            if(i+1 >= threadsCount && nextSteps < result.length){
+                nextSteps = result.length;
+                Totalizer count = new Totalizer(threadsCount, tailSteps, nextSteps, result, summa);
+                total.add(count);
+                break;
+            }
+            Totalizer count = new Totalizer(i+1, tailSteps, nextSteps, result, summa);
+            total.add(count);
+            if(nextSteps + steps >= result.length) {
+                tailSteps = nextSteps;
+                nextSteps = result.length;
+            }
+            else{
+                tailSteps = nextSteps;
+                nextSteps += steps;
             }
         }
-        System.out.println(Arrays.toString(steps));
-        System.out.println(Arrays.toString(result));
+        return total;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        int arraySize;
+        int threadsCount;
+        try {
+             arraySize = parserInt(args[0]);
+             threadsCount = parserInt(args[1]);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Аргументы отсутствуют " + e.getMessage());
+            arraySize = 10;
+            threadsCount = 3;
+        }
+        int[] result = generator(arraySize);
+        int steps = step(arraySize, threadsCount);
+        Summa summa = new Summa(0);
+        ArrayList<Totalizer> total = total(steps, threadsCount, result, summa);
+
+        for (Totalizer count : total){
+            Thread thread = new Thread(count);
+            thread.start();
+            thread.join();
+        }
+
+
         System.out.println("checkSum = " + checkSum(result));
         System.out.println("SummaThreads = " + summa.getSumma());
+//        System.out.println(Arrays.toString(result));
     }
 }

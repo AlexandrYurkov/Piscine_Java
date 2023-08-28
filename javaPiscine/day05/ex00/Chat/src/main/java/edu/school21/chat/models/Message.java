7859bfc6caaad8389cbb366dbbe53ce9d8c066ee
aleksandryurkov <Aleksandr.Yurkov@infotecs.ru>
2023-08-28 18:02:49 +0300

    ex00

---
+++ javaPiscine/day05/ex00/Chat/src/main/java/edu/school21/chat/models/Message.java
@@ -0,0 +1,68 @@
+package src.main.java.edu.school21.chat.models;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Message {
+    private final long idMessage;
+    private final User authorMessage;
+    public ChatRoom chatRoom;
+    public String message;
+    public Date date;
+
+    public Message(long idMessage, User authorMessage, ChatRoom chatRoom, String message) {
+        this.idMessage = idMessage;
+        this.authorMessage = authorMessage;
+        this.chatRoom = chatRoom;
+        this.message = message;
+        this.date = new Date();
+    }
+
+    public long getIdMessage() {
+        return idMessage;
+    }
+
+    public User getAuthorMessage() {
+        return authorMessage;
+    }
+
+    public ChatRoom getChatRoom() {
+        return chatRoom;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message1 = (Message) o;
+        return idMessage == message1.idMessage && Objects.equals(authorMessage, message1.authorMessage) && Objects.equals(chatRoom, message1.chatRoom) && Objects.equals(message, message1.message) && Objects.equals(date, message1.date);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idMessage, authorMessage, chatRoom, message, date);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "idMessage=" + idMessage +
+                ", authorMessage=" + authorMessage +
+                ", chatRoom=" + chatRoom +
+                ", message='" + message + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ javaPiscine/day05/ex00/Chat/src/main/java/edu/school21/chat/models/TestProgram.java
@@ -0,0 +1,7 @@
+package src.main.java.edu.school21.chat.models;
+
+public class TestProgram {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
